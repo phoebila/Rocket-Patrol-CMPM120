4bcf9a22db70ed8edfe89d0a98c4a2f933c9908a
Phoebe <128556139+phoebila@users.noreply.github.com>
2024-01-13 14:36:50 -0800

    Added assets and moving background

--- Rocket Patrol/src/scenes/Menu.js
+++ Rocket Patrol/src/scenes/Menu.js
@@ -3,7 +3,15 @@ class Menu extends Phaser.Scene {
         super("menuScene")
     }
 
+    preload() {
+        // load images/tile sprites
+        this.load.image('rocket', '/Rocket Patrol/assets/rocket.png')
+        this.load.image('spaceship', '/Rocket Patrol/assets/spaceship.png')
+        this.load.image('starfield', '/Rocket Patrol/assets/starfield.png')
+      }
+
     create() {
         this.add.text(20, 20, "Rocket Patrol Menu")
+        this.scene.start("playScene")
       }
 }
