3a58badc8568dd125b10945fc8d9cf2550a9c0aa
Phoebe <128556139+phoebila@users.noreply.github.com>
2024-01-13 14:09:35 -0800

    Got Rocket Patrol Menu

--- Rocket Patrol/src/main.js
+++ Rocket Patrol/src/main.js
@@ -1,1 +1,11 @@
-console.log("hello world")
+// console.log("hello world")
+
+let config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 480,
+    scene: [Menu, Play]
+  }
+
+let game = new Phaser.Game(config);
+
